0f37fa25e600f0c3ab51d3f9da00b1c5bfdbf041
Naomi Flagg <neffly511@gmail.com>
2020-10-28 10:47:32 -0400

    Link sendgrid for mailers

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :facebook
 
   def facebook
-    flash[:notice] = 'Sorry, Fakebook is not actually live on Facebook, so this feature only works in development.'
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "fakebook_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'polar-island-23304.herokuapp.com/' }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://polar-island-23304.herokuapp.com' }
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
